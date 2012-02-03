81faac20ce8aefdd10fa4b5fd0cf87df5cbbcbf4
Matt Furden <zolrath@gmail.com>
2012-02-03 01:04:45 -0800

    Added if __FILE__ == -/bin/zsh

--- lib/marky-markov.rb
+++ lib/marky-markov.rb
@@ -2,9 +2,11 @@
 require_relative 'marky-markov/markov-dictionary'
 require_relative 'marky-markov/sentence-generator'
 
-file = ARGV[0] || "frank.txt"
-wordcount = ARGV[1] || 200
+if __FILE__ == $0
+  file = ARGV[0] || "frank.txt"
+  wordcount = ARGV[1] || 200
 
-dict = MarkovDictionary.new(file)
-sentence = SentenceGenerator.new(dict.dictionary)
-puts sentence.generate(wordcount.to_i)
+  dict = MarkovDictionary.new(file)
+  sentence = SentenceGenerator.new(dict.dictionary)
+  puts sentence.generate(wordcount.to_i)
+end
