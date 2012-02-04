dee244958c04d09fecef5958d8b44e0f25509900
Matt Furden <zolrath@gmail.com>
2012-02-04 05:08:31 -0800

    Add To Dict actually adds stuff. Some files fail
    Not sure whats making certain files cause the dictionary to fail to add
    them. + is undefined for nil class, but how is it getting a nil? Time to
    find out.

--- lib/add-to-dict.rb
+++ lib/add-to-dict.rb
@@ -4,7 +4,8 @@ require_relative 'marky-markov/persistent-dictionary'
 
 if __FILE__ == $0
   unless ARGV[0].nil?
-    pers_dict = PersistentDictionary.new(ARGV[0], 'dictionary')
+    pers_dict = PersistentDictionary.new('dictionary')
+    pers_dict.parse_source(ARGV[0])
     pers_dict.save_dictionary!
   end
 end
