16b27a58cabfc78a900fb5ff7ad520dae2bac9b6
Matt Furden <zolrath@gmail.com>
2012-02-04 03:27:34 -0800

    Add last two words of source into dictionary.

--- lib/marky-markov/two-word-dictionary.rb
+++ lib/marky-markov/two-word-dictionary.rb
@@ -1,12 +1,12 @@
 require_relative 'markov-dictionary'
 
 class TwoWordDictionary < MarkovDictionary
-  def parse_file
-    # Special case for last word in source file as it has no words following it.
-    @contents = open_file(@filename)
+  def parse_source(source)
+    @contents = open_source(source)
     (@contents.length-3).times do |i|
       rootword = "#{@contents[i]} #{@contents[i+1]}"
       self.add_word(rootword, @contents[i+2])
     end
+    @dictionary[@contents.last(2).join(' ')] ||= Hash.new(0)
   end
 end
