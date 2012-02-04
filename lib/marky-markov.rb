aef966a6d802ad989a33d6bcd852129eb5c58ab3
Matt Furden <zolrath@gmail.com>
2012-02-04 03:19:09 -0800

    Main runtime can use PersistentDictionary
    Marky-Markov can build a dictionary based on a supplied file for one
    time use or if left blank will build a sentence based on the
    PersistentDictionary, if present.

--- lib/marky-markov.rb
+++ lib/marky-markov.rb
@@ -1,14 +1,23 @@
 #!/usr/bin/env ruby -i
 require_relative 'marky-markov/markov-dictionary'
-require_relative 'marky-markov/sentence-generator'
 require_relative 'marky-markov/two-word-dictionary'
+require_relative 'marky-markov/persistent-dictionary'
+require_relative 'marky-markov/sentence-generator'
 require_relative 'marky-markov/two-word-sentence-generator'
 
 if __FILE__ == $0
-  file = ARGV[0] || "frank.txt"
-  wordcount = ARGV[1] || 200
+  wordcount = ARGV[0] || 200
+  source = ARGV[1]
 
-  dict = TwoWordDictionary.new(file)
+  if source.nil?
+    if File.exists?('dictionary')
+      dict = PersistentDictionary.new('dictionary')
+    else
+      puts "No source text or dictionary supplied."
+    end
+  else
+    dict = TwoWordDictionary.new(source)
+  end
   sentence = TwoWordSentenceGenerator.new(dict.dictionary)
   puts sentence.generate(wordcount.to_i)
 end
