c31746c2189a96f268f4fb5652da03a9aef12c12
Matt Furden <zolrath@gmail.com>
2012-02-04 15:41:40 -0800

    Make content into a local variable.

--- lib/marky-markov/markov-dictionary.rb
+++ lib/marky-markov/markov-dictionary.rb
@@ -23,10 +23,10 @@ class MarkovDictionary
 
   def parse_source(source)
     # Special case for last word in source file as it has no words following it.
-    @contents = open_source(source)
-    (@contents.length-1).times do |i|
-      self.add_word(@contents[i], @contents[i+1])
+    contents = open_source(source)
+    (contents.length-1).times do |i|
+      self.add_word(contents[i], contents[i+1])
     end
-    @dictionary[(@contents.last)] ||= Hash.new(0)
+    @dictionary[(contents.last)] ||= Hash.new(0)
   end
 end
--- lib/marky-markov/two-word-dictionary.rb
+++ lib/marky-markov/two-word-dictionary.rb
@@ -2,11 +2,11 @@ require_relative 'markov-dictionary'
 
 class TwoWordDictionary < MarkovDictionary
   def parse_source(source)
-    @contents = open_source(source)
-    (@contents.length-3).times do |i|
-      rootword = "#{@contents[i]} #{@contents[i+1]}"
-      self.add_word(rootword, @contents[i+2])
+    contents = open_source(source)
+    (contents.length-3).times do |i|
+      rootword = "#{contents[i]} #{contents[i+1]}"
+      self.add_word(rootword, contents[i+2])
     end
-    @dictionary[@contents.last(2).join(' ')] ||= Hash.new(0)
+    @dictionary[contents.last(2).join(' ')] ||= Hash.new(0)
   end
 end
